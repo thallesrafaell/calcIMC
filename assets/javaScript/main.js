d66dcdfaeb64b7658f9bd312ad8e98e0dc8537ff
Thalles Rafael <thallesrafaell@outlook.com>
2023-08-31 00:18:30 -0300

    RETIRANDO BUG DA ESTRUTURA CONDICIONAL

--- assets/javaScript/main.js
+++ assets/javaScript/main.js
@@ -2,7 +2,6 @@ const form = document.getElementById('form1')
 form.addEventListener('submit', function(e){
     e.preventDefault()
     attres()
-    calcImc(peso, altura)
 }) 
 
 function  attres(){
@@ -20,23 +19,23 @@ function  attres(){
             img.setAttribute('src', 'assets/images/homemmabp.png')
             res.textContent = `O seu imc e de ${imcres}, você está muito abaixo do peso indicado`
 
-        }  if(imcres >= 17 && imcres < 18,5) {
+        } else if(imcres >= 17 && imcres < 18.5) {
             img.setAttribute('src', 'assets/images/homemabp.png')
             res.textContent = `O seu imc e de ${imcres}, você está abaixo do peso indicado`
 
-        } if(imcres >= 18,5 && imcres < 25) {
+        } else if(imcres >= 18.5 && imcres < 25) {
             img.setAttribute('src', 'assets/images/homemnormal.png')
             res.textContent = `O seu imc e de ${imcres}, parabéns você está com peso indicado`
 
-        } if(imcres >= 25  && imcres < 30) {
+        }else if(imcres >= 25  && imcres < 30) {
             img.setAttribute('src', 'assets/images/homemacp.png')
             res.textContent = `O seu imc e de ${imcres}, você está acima do peso indicado`
 
-        } if (imcres >= 30 && imcres < 35) {
+        }else if (imcres >= 30 && imcres < 35) {
             img.setAttribute('src', 'assets/images/homemob1.png')
             res.textContent = `O seu imc e de ${imcres}, você esta com obesidade Grau I`
 
-        } if(imcres >= 35 && imcres < 40) {
+        }else if(imcres >= 35 && imcres < 40) {
             img.setAttribute('src', 'assets/images/homemob2.png')
             res.textContent = `O seu imc e de ${imcres}, você esta com obesidade Grau II`
 
@@ -47,19 +46,19 @@ function  attres(){
         
     } else if(sex[1].checked){
         if( imcres < 17) {
-            img.setAttribute('src', 'assets/images/mulhermabp.png')
+            img.setAttribute('src', 'assets/images/mulhermab.png')
             res.textContent = `O seu imc e de ${imcres}, você está muito abaixo do peso indicado`
 
-        }else if(imcres >= 17 && imcres < 18,5) {
-            img.setAttribute('src', 'assets/images/mulherabp.png')
+        }else if(imcres >= 17 && imcres < 18.5) {
+            img.setAttribute('src', 'assets/images/mulherab.png')
             res.textContent = `O seu imc e de ${imcres}, você está abaixo do peso indicado`
 
-        }else if(imcres >= 18,5 && imcres < 25) {
+        }else if(imcres >= 18.5 && imcres < 25) {
             img.setAttribute('src', 'assets/images/mulhernormal.png')
             res.textContent = `O seu imc e de ${imcres}, parabéns você está com peso indicado`
 
         }else if(imcres >= 25  && imcres < 30) {
-            img.setAttribute('src', 'assets/images/mulheracp.png')
+            img.setAttribute('src', 'assets/images/mulherap.png')
             res.textContent = `O seu imc e de ${imcres}, você está acima do peso indicado`
 
         } else if (imcres >= 30 && imcres < 35) {
@@ -79,9 +78,6 @@ function  attres(){
 }
 
 
-function calcImc(peso, altura){
-    let imc = 0
-    return imc = peso / (altura*altura)
+function calcImc(peso, altura){ 
+    return  peso / (altura*altura)
 }
-
-dres.st
